da4294552cd6ac822322e6c87d38e1750d4e5964
dushyantsingh149 <98822190+dushyantsingh149@users.noreply.github.com>
2022-05-18 21:36:17 +0530

    pep8

--- main.py
+++ main.py
@@ -1,69 +1,58 @@
 from game_data import data
-from art import logo,vs
+from art import logo, vs
 import random
 
 
-
-
 def pickRandom():
-    
-    item=random.choice(data)
-   
+
+    item = random.choice(data)
+
     return item
 
 
-def show(a,b):
-    
-    
+def show(a, b):
+
     print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
     print()
     print(vs)
     print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}")
     print()
-    
-def getPop(a,b):
-    
-    if a['follower_count']>=b['follower_count']:
+
+
+def getPop(a, b):
+
+    if a['follower_count'] >= b['follower_count']:
         return 'a'
-    
+
     else:
         return 'b'
-    
+
+
 def game():
-    
-    a={}
-    b=pickRandom()
-    score=0
+
+    a = {}
+    b = pickRandom()
+    score = 0
     print(logo)
-    
+
     while True:
-        
-        a=b
-        b=pickRandom()
-        
+
+        a = b
+        b = pickRandom()
+
         show(a, b)
-        
-        ch=input("Who has more followers? Type 'A' or 'B': ").lower()
-        
-       
-        
-        
-        if ch==getPop(a, b):
-            score+=1
-            
+
+        ch = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+        if ch == getPop(a, b):
+            score += 1
+
             print(f"You're right! Current score: {score}")
-            
+
         else:
-            
+
             print(f"Sorry, that's wrong. Final score: {score}")
             break
-            
-            
-            
-game() 
-            
-        
-        
-        
-        
-        
+
+
+game()
